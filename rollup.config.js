a2e58506e61ceee11abb4a599413b22af7b46aca
Miguel Duarte <miguelfeliped@gmail.com>
2020-05-16 21:25:50 +0200

    Compiling paymail client in the same way than the other packages. (#3953)
    Co-authored-by: Ryan X. Charles <ryanxcharles@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,15 @@
 import babel from 'rollup-plugin-babel'
+import builtins from 'rollup-plugin-node-builtins'
+import commonJS from '@rollup/plugin-commonjs'
+import fs from 'fs'
+import globals from 'rollup-plugin-node-globals'
+import { terser } from 'rollup-plugin-terser'
 import path from 'path'
 import pkg from './package.json'
+import replace from '@rollup/plugin-replace'
+import resolve from '@rollup/plugin-node-resolve'
 
-function isExternal (candidate) {
-  return Object.keys(pkg.dependencies).some(dependency => {
-    return candidate.startsWith(dependency)
-  })
-}
+const PRODUCTION_BUILD = process.env.NODE_ENV === 'production'
 
 export default [
   {
@@ -25,24 +28,105 @@ export default [
       }
     ],
     plugins: [
+      replace(getReplacements()),
       babel({
-        runtimeHelpers: true,
-        exclude: ['node_modules/**'],
         presets: [
           [
             '@babel/preset-env',
             {
               modules: false,
               targets: {
-                node: 'current'
+                node: '10'
               }
             }
           ]
         ],
-        plugins: [
-          '@babel/plugin-transform-runtime'
-        ]
+        plugins: getBabelPlugins()
       })
     ]
-  }
+  },
+  ...(PRODUCTION_BUILD ? [{
+    input: path.resolve(__dirname, 'src', 'index.js'),
+    output: [
+      {
+        file: pkg.unpkg,
+        format: 'iife',
+        name: pkg.library,
+        sourcemap: true
+      }
+    ],
+    context: 'window',
+    plugins: [
+      replace(getReplacements()),
+      globals(),
+      builtins(),
+      resolve({
+        browser: true,
+        preferBuiltins: true
+      }),
+      commonJS({
+        namedExports: {
+          '../../node_modules/loglevel/lib/loglevel.js': [
+            'setLevel',
+            'trace',
+            'debug',
+            'info',
+            'warn',
+            'error'
+          ]
+        }
+      }),
+      babel({
+        exclude: ['../../node_modules/**', 'node_modules/**'],
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              modules: false,
+              targets: {
+                browsers: ['> 2%']
+              }
+            }
+          ]
+        ],
+        runtimeHelpers: true,
+        plugins: getBabelPlugins({ includeTransformRuntime: true })
+      }),
+      terser({
+        sourcemap: true,
+        output: {
+          preamble: getBanner()
+        }
+      })
+    ]
+  }] : [])
 ]
+
+function isExternal (candidate) {
+  return Object.keys(pkg.dependencies).some(dependency => {
+    return candidate.startsWith(dependency)
+  })
+}
+
+function getBanner () {
+  const filePath = path.resolve(__dirname, 'src', 'banner.js')
+  return fs.readFileSync(filePath).toString().trim()
+}
+
+function getBabelPlugins (options = {}) {
+  const plugins = [
+    '@babel/plugin-proposal-object-rest-spread'
+  ]
+  if (options.includeTransformRuntime) {
+    plugins.push('@babel/plugin-transform-runtime')
+  }
+  return plugins
+}
+
+function getReplacements () {
+  const replacements = {}
+  for (const key in process.env) {
+    replacements[`process.env.${key}`] = JSON.stringify(process.env[key])
+  }
+  return replacements
+}
