54dc9c03e63cdbbe4ebe73cfdb22f89242b33c62
elijahb <maurolewinzon@gmail.com>
2021-07-15 08:55:09 -0600

    Fix tests

--- src/dns-client.js
+++ src/dns-client.js
@@ -88,7 +88,7 @@ class DnsClient {
     const port = data[2]
     const responseDomain = data[3]
     if (!dnsResponse.AD && !this.domainsAreEqual(aDomain, responseDomain)) {
-      throw new PaymailServerError(`${aDomain} is not correctly configured: insecure domain.`)
+      throw new PaymailServerError(`${aDomain} is not correctly configured: insecure domain`)
     }
     return {
       port,
