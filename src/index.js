01a3b10fba86fdbab60523cd378716396afba7b1
elijahb <maurolewinzon@gmail.com>
2022-08-31 09:36:16 -0300

    Refactored worker, added Sentry, added tests (#4869)
    * Refactored worker, added Sentry, added tests
    * Added taal api key to env-test-ci
    * Add default sentry dns for local dev

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { CapabilityCodes } from './constants'
 import { AssetNotAccepted } from './errors/AssetNotAccepted'
 import { ProtocolNotSupported } from './errors/ProtocolNotSupported'
 import { PaymailServerError } from './errors/PaymailServerError'
+import { EndpointResolver } from './EndpointResolver'
 
 export {
   PaymailClient,
@@ -19,5 +20,6 @@ export {
   CapabilityCodes,
   AssetNotAccepted,
   ProtocolNotSupported,
-  PaymailServerError
+  PaymailServerError,
+  EndpointResolver
 }
